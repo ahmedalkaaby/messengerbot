5ac6eba494534fd5d6a86e12bbf1ca7e987b8b14
K3BE <ahmed.13.alkaaby@gmail.com>
2020-04-18 18:26:58 +0300

    first Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const facebook = require("fb-messenger-bot-api");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+//allow us to process the data
+app.use(bodyParser.urlencoded({ textended: false }));
+app.use(bodyParser.json());
+
+//Routes
+app.get("/", function (req, res) {
+  res.send("Hi, Im Ahmed.......");
+});
+
+// Facebook
+app.get("/webhook/", function (req, res) {
+  if (req.query["hub.verify_token"] === "ahmed&qamar") {
+    res.send(req.query["hub.challenge"]);
+  }
+  res.send("ERROR! Wrong Token");
+});
+
+app.listen(app.get("port"), function () {
+  console.log("running: port");
+});
