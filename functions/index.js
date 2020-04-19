ad5d50e42e5f95d6d41d777a191689590b00739c
K3BE <ahmed.13.alkaaby@gmail.com>
2020-04-19 21:41:32 +0300

    update

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const facebook = require("fb-messenger-bot-api");
+// const functions = require("firebase-functions");
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
@@ -13,12 +13,13 @@ app.use(bodyParser.json());
 
 //Routes
 app.get("/", function (req, res) {
+  console.log("hello");
   res.send("Hi, Im Ahmed.. :)");
 });
 
 // Facebook
 app.get("/webhook/", function (req, res) {
-  if (req.query["hub.verify_token"] === "ahmed&qamar") {
+  if (req.query["hub.verify_token"] === "nabeelbot") {
     res.send(req.query["hub.challenge"]);
   }
   res.send("ERROR! Wrong Token");
@@ -27,3 +28,5 @@ app.get("/webhook/", function (req, res) {
 app.listen(app.get("port"), function () {
   console.log("running: port");
 });
+
+// exports.app = functions.https.onRequest(app);
